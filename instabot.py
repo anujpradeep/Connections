14aa9ff3fd52fb3c40ba7afd98a3204aeca28ecf
anujpradeep <59856247+anujpradeep@users.noreply.github.com>
2020-01-19 01:17:28 -0700

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -10,27 +10,12 @@ class Follow:
 		self.username = username
 		self.driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
 		sleep(2)
-		#self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
-		#	.click()
-		sleep(2)
 		self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
 		self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
 		keyboard.press(Key.enter)
 		keyboard.release(Key.enter)
 		sleep(4)
-
-	# 	#sleep(10) #need to use to open the with serc. code
-	# 	self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
-	# 	sleep(2)
 	def followuser(self,username2):
-	# 	self.driver.find_element_by_xpath("//input[@placeholder='Search']").send_keys(username2)
-	# 	sleep(4)
-	# 	keyboard.press(Key.enter)
-	# 	keyboard.release(Key.enter)
-	# 	sleep(1)
-
-	# 	keyboard.press(Key.enter)
-	# 	keyboard.release(Key.enter)
 		self.driver.get("https://www.instagram.com/"+username2+"/")
 		sleep(4)
 		keyboard.press(Key.tab)
@@ -40,9 +25,7 @@ class Follow:
 		keyboard.press(Key.enter)
 		keyboard.release(Key.enter)
 		
-#class Follow:
-
-#pw = 'y6bs^Vm^RKT$tD?'
+		
 pw = 'thisisatest'
 
 username = 'ryans_fire'
